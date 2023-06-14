e24a383606742ce8141abd58c795e8da945b02e0
gaurav-namboodri <55475529+gaurav-namboodri@users.noreply.github.com>
2023-06-14 08:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-	
-  return key in sampleObject;
 
 console.log(hasKey("white")); // true
 console.log(hasKey("green")); // true
